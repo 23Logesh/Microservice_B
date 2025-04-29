d17b166119ce95327299797cb49be0374268291a
Logeshwaran <140958412+23Logesh@users.noreply.github.com>
2025-04-29 09:56:33 +0530

    init

---
+++ src/main/java/com/example/Service_B/entity/BikeEntityB.java
@@ -0,0 +1,21 @@
+package com.example.Service_B.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class BikeEntityB {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int bikeNumber;
+	private String bikeName;
+	private String bikeModel;
+	private String bikeType;
+
+
+}
